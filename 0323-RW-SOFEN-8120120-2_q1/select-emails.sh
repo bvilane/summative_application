9eb973e601f4d372cb8f60fb9aae02957d3467d3
Geu Aguto Garang <133713642+Geu-Pro2023@users.noreply.github.com>
2023-06-21 09:35:40 +0200

    Updated select-emails.sh

--- 0323-RW-SOFEN-8120120-2_q1/select-emails.sh
+++ 0323-RW-SOFEN-8120120-2_q1/select-emails.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Select just the emails of the student and save them in a file “student-emails.txt”
-  cut -d ',' -f 1 students-list_0333.txt > student-emails.txt
+  cut -d ',' -f 1 students-list_0333.txt>student-emails.txt
 
   echo "All emails saved in student-emails.txt:"
 cat student-emails.txt
